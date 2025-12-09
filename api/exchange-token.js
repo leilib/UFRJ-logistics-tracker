dd63c3a9a1f91b7b7a8e6aa0aa2da2efa30610ba
leilib <139494553+leilib@users.noreply.github.com>
2025-12-09 12:01:28 -0300

    Add CORS support to exchange-token.js
    Add CORS headers to exchange token API

--- api/exchange-token.js
+++ api/exchange-token.js
@@ -1,3 +1,7 @@
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
